1fe35c4e0ba16b3386a78d32ce739dfeced31667
ProgrammerJarvis <rankaarvids@gmail.com>
2022-03-03 10:13:17 +0200

    just some changes again#

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from ast import match_case
+
+
 print("jidfgyasubfoyagfaiw")
 
 push = 6456456
@@ -5,7 +8,10 @@ push = 6456456
 no = 56
 
 math = push + no
-print(math)
 
 for i in range(math):
     print(i)
+
+print(math)
+
+"insert here"
